fbd05475ba5f9252d2e41f321b1d687dca557322
jnzhang622 <jnzhang622@gmail.com>
2020-05-01 11:47:53 -0400

    deliverables

---
+++ app/models/appearance.rb
@@ -0,0 +1,6 @@
+class Appearance < ApplicationRecord
+    belongs_to :episode
+    belongs_to :guest
+
+    validates :rating, numericality: {greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
+end
